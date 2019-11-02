ef60cc4b4989cf7712f75b2bd2bd978360401bfb
Sergey <sergey.rymar.ne@yandex.ru>
2019-11-02 18:40:58 +0200

    add UserRest class and add to UserController class method getUsers () with @RequestParam

--- src/main/java/com/rymar/app/ws/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/rymar/app/ws/mobileappws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.rymar.app.ws.mobileappws.ui.controller;
 
+import com.rymar.app.ws.mobileappws.ui.model.response.UserRest;
 import org.springframework.web.bind.annotation.*;
 
 /*
@@ -7,11 +8,21 @@ import org.springframework.web.bind.annotation.*;
  *autor Sergey on 02.11.2019 16:14
  */
 @RestController
-@RequestMapping("users")//http://localhost:8080/users
+@RequestMapping("/users")//http://localhost:8080/users
 public class UserController {
     @GetMapping
-    public String getUser(){
-        return " get user was colled ";
+    public String getUsers(@RequestParam(value = "page",defaultValue = "1") int page,
+                           @RequestParam(value = "limit",defaultValue = "25") int limit,
+                           @RequestParam(value = "sort",required = false) String sort ){
+        return " get users with page="+page+" limit="+limit +" sort ="+sort;
+    }
+    @GetMapping(path = "/{userId}")
+    public UserRest getUser(@PathVariable String userId){
+        UserRest userRest = new UserRest();
+        userRest.setEmail("test@test.ua");
+        userRest.setFirstName("Sergey");
+        userRest.setLastName("Rymar");
+        return userRest;
     }
     @PostMapping
     public String createUser(){
---
+++ src/main/java/com/rymar/app/ws/mobileappws/ui/model/response/UserRest.java
@@ -0,0 +1,18 @@
+package com.rymar.app.ws.mobileappws.ui.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/*
+ *
+ *autor Sergey on 02.11.2019 18:01
+ */
+@Getter
+@Setter
+public class UserRest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userId;
+
+}
